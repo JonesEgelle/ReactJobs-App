980aaad939d5920d9fa4f6750c5d496dc854a55f
JonesEgelle <egellejones@gmail.com>
2026-01-21 13:32:47 +0100

    feat: React Job Application featuring CRUD (create,read,update,delete). Techstack: React/Vite, JSON server (Backend), React Router, React Toastify.

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,81 @@
-import React from "react";
-import Navbar from "./components/navBar";
-import Hero from "./components/hero";
-import HomeCards from "./components/homeCards";
-import JobListings from "./components/jobListings";
-import ViewAllJobs from "./components/viewAllJobs";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router";
+import HomePage from "./pages/HomePage";
+import MainLayout from "./layout/MainLayout";
+import JobsPage from "./pages/JobsPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import AddJobPage from "./pages/AddJob";
+import EditJobPage from "./pages/EditJobPage";
+import JobPage, { jobloader } from "./pages/JobPage";
 
 //Main App Component
 const App = () => {
+  //AddJob function
+  const addNewJob = async (newJob) => {
+    const res = await fetch("/api/jobs/", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(newJob),
+    });
+    return;
+  };
+
+  //DeleteJob function
+  const deleteJob = async (id) => {
+    const res = await fetch(`/api/jobs/${id}`, {
+      method: "DELETE",
+    });
+
+    console.log("Delete", id);
+    return;
+  };
+
+  //EditJob function
+  const editJob = async (job) => {
+    const res = await fetch(`/api/jobs/${job.id}`, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(job),
+    });
+
+    console.log("Update", job);
+    return;
+  };
+  //Routing system
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/jobs" element={<JobsPage />} />
+        <Route
+          path="/jobs/:id"
+          element={<JobPage deleteJob={deleteJob} />}
+          loader={jobloader}
+        />
+        <Route
+          path="/add-job"
+          element={<AddJobPage addJobSubmit={addNewJob} />}
+        />
+        <Route
+          path="/edit-job/:id"
+          element={<EditJobPage updatedJobSubmit={editJob} />}
+          loader={jobloader}
+        />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    )
+  );
   return (
     <>
-      <Navbar />
-      <Hero
-        title="Become a React Dev"
-        subtitle="Find the React job that fits your skills and needs"
-      />
-      <HomeCards />
-      <JobListings />
-      <ViewAllJobs />
+      <RouterProvider router={router} />
     </>
   );
 };
--- src/components/jobListings.jsx
+++ src/components/jobListings.jsx
@@ -1,20 +1,45 @@
-import jobs from "../jobs.json";
-import Joblisting from "./joblisting";
+import JobListing from "./joblisting";
+import { useEffect, useState } from "react";
+import Spinners from "./Spinners";
 
-const JobListings = () => {
-  const RecentJobs = jobs.slice(0, 3);
+const JobListings = ({ isHome = false }) => {
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState();
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      const apiUrl = isHome ? "/api/jobs?_limit=3" : "/api/jobs";
+      try {
+        const res = await fetch(apiUrl);
+        const data = await res.json();
+        setJobs(data);
+      } catch (error) {
+        console.log("error fetching data", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchJobs();
+  }, []);
+
+  // const jobListing = isHome ? jobs.slice(0, 3) : jobs;
 
   return (
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-          Browse Jobs
+          {isHome ? "Recent Jobs" : "Browse Jobs"}
         </h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {RecentJobs.map((job) => (
-            <Joblisting key={job.id} job={job} /> // Pass job object as prop
-          ))}
-        </div>
+
+        {loading ? (
+          <Spinners loading={loading} />
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {jobs.map((job) => (
+              <JobListing key={job.id} job={job} /> // Pass job object as prop
+            ))}
+          </div>
+        )}
       </div>
     </section>
   );
