14944d9cf30e19bb68874d2bdf74b3157eb76479
JonesEgelle <egellejones@gmail.com>
2026-01-22 10:04:28 +0100

    feat: bug fix ([vite]: Rollup failed to resolve import "react-spinners/Cliploader" from "/vercel/path0/src/components/Spinners.jsx")

--- src/components/Spinners.jsx
+++ src/components/Spinners.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { ClipLoader } from "react-spinners/Cliploader";
+import { ClipLoader } from "react-spinners";
 
 const Spinners = ({ loading }) => {
   const override = {
